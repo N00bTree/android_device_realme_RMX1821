f2d9f3ab136a01e344d34bf5acec36b336d75876
chankruze <chankruze@gmail.com>
2019-07-08 14:07:36 +0530

    Adopted for Pitch Black Recovery Project
    Signed-off-by: chankruze <chankruze@gmail.com>

--- omni_spartan.mk
+++ omni_spartan.mk
@@ -4,8 +4,10 @@ PRODUCT_RELEASE_NAME := spartan
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Realme
